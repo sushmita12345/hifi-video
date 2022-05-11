dd537fd4aba5a65b6643c01f7fbc9372eab73fd0
sushmita12345 <38626023+sushmita12345@users.noreply.github.com>
2022-05-12 02:14:28 +0530

    added more data in backend

--- src/Utils/ViewsConverter.jsx
+++ src/Utils/ViewsConverter.jsx
@@ -1,8 +1,8 @@
 export const ViewsConverter = (num) => {
-    if(num < 900){
+    if(num < 999){
         return num
     }else if(num > 999 && num < 1000000){
-        return (num / 1000) + "K"
+        return (num / 1000).toFixed(1) + "K"
     }else if(num > 1000000){
         return (num / 1000000).toFixed(1) + "M"
     }
--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -7,7 +7,7 @@ import { thumbnail, ViewsConverter } from "../../Utils/index";
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
+    _id: "kDd33VxaxaY",
     title: "Google Frontend Interview Questions",
     videoLink: "https://www.youtube.com/watch?v=kDd33VxaxaY",
     description:
@@ -21,7 +21,7 @@ export const videos = [
   },
 
   {
-    _id: "Wo5dMEP_BbI",
+    _id: "noxC-YCfuJw",
     title: "Top 5 tips for your JavaScript interview (from a Facebook Engineer)",
     videoLink: "https://www.youtube.com/watch?v=noxC-YCfuJw&t=1s",
     description:
@@ -35,7 +35,7 @@ export const videos = [
   },
 
   {
-    _id: "Wo5dMEP_BbI",
+    _id: "AX6ECLcJzps",
     title: "Amazon Star Rating Interview Question in React | Front End",
     videoLink: "https://www.youtube.com/watch?v=AX6ECLcJzps",
     description:
@@ -49,7 +49,7 @@ export const videos = [
   },
 
   {
-    _id: "Wo5dMEP_BbI",
+    _id: "abbdJ4Yfm54",
     title: "Frontend Interview Experience (Unacademy) - Javascript and React JS Interview Questions",
     videoLink: "https://www.youtube.com/watch?v=abbdJ4Yfm54&t=19s",
     description:
@@ -63,7 +63,7 @@ export const videos = [
   },
 
   {
-    _id: "Wo5dMEP_BbI",
+    _id: "vxggZffOqek",
     title: "Frontend Interview Experience (Cars24) - Javascript and React JS Interview Questions",
     videoLink: "https://www.youtube.com/watch?v=vxggZffOqek&t=1388s",
     description:
@@ -74,5 +74,75 @@ export const videos = [
     thumbnail: thumbnail("vxggZffOqek"),
     view: ViewsConverter(228000),
     timeStamp: "01/23/2022"
+  },
+
+  {
+    _id: "ai1zmNO5Z3E",
+    title: "Google Frontend Interview With A Frontend Expert",
+    videoLink: "https://www.youtube.com/watch?v=ai1zmNO5Z3E&t=513s",
+    description:
+      "In this video, I conduct a mock Google frontend interview with a frontend expert, Conner Ardman, who's also the FrontendExpert course creator and an ex-Facebook software engineer. As a Google Software Engineer, I interviewed dozens of candidates. This is exactly the type of frontend coding interview that you would get at Google or any other big tech company.",
+    creator: "Clément Mihailescu",
+    categoryName: "Google",
+    img: "https://yt3.ggpht.com/ytc/AKedOLRhv8ie_8iIxaL7uSIp5OVbZCxus2at2O7q5fowxCE=s176-c-k-c0x00ffffff-no-rj",
+    thumbnail: thumbnail("ai1zmNO5Z3E"),
+    view: ViewsConverter(298000),
+    timeStamp: "04/13/2022"
+  },
+
+  {
+    _id: "WiOMzIUZXl0",
+    title: "Google frontend Interview Experience | apart from DSA what else does Google ask in the interview?",
+    videoLink: "https://www.youtube.com/watch?v=WiOMzIUZXl0",
+    description:
+      "To watch more videos on programming, Data Structures, Android Development, Data Science, C++, Java, React, subscribe to our channel.",
+    creator: "Think Like A Coder | Newton school",
+    categoryName: "Google",
+    img: "https://yt3.ggpht.com/tjsk3duhcGFbFZdiWzjYKTPo2kx_dofs6OvHMi4y8eLdxausUfotRGj_ebQjBMBGUmLN0T57sg=s176-c-k-c0x00ffffff-no-rj",
+    thumbnail: thumbnail("WiOMzIUZXl0"),
+    view: ViewsConverter(963),
+    timeStamp: "03/14/2022"
+  },
+
+  {
+    _id: "BVNSk53CCI4",
+    title: "Google Job Interview - Front End Engineer",
+    videoLink: "https://www.youtube.com/watch?v=BVNSk53CCI4",
+    description:
+      "I interviewed for a front end engineer position at Google. This is my experience from first contact to on site interviews!",
+    creator: "Jesse Weigel",
+    categoryName: "Google",
+    img: "https://yt3.ggpht.com/ytc/AKedOLRP_vrnVUmFrKMsFpfgtbSyYdEXe4TCyKQ4VIZgcg=s176-c-k-c0x00ffffff-no-rj",
+    thumbnail: thumbnail("BVNSk53CCI4"),
+    view: ViewsConverter(18039),
+    timeStamp: "11/27/2018"
+  },
+
+  {
+    _id: "rMWDtxJQIbQ",
+    title: "Amazon Front End Interview Prep | Software Engineer",
+    videoLink: "https://www.youtube.com/watch?v=rMWDtxJQIbQ",
+    description:
+      "In this video, I discuss how I prepared for my Amazon Interview in New York. I ended up receiving an offer for a Front End Engineer II position, which I accepted. This type of preparation would be the same regardless of any company I applied for. Hope this helps!",
+    creator: "TechRally",
+    categoryName: "Amazon",
+    img: "https://yt3.ggpht.com/ytc/AKedOLRP_vrnVUmFrKMsFpfgtbSyYdEXe4TCyKQ4VIZgcg=s176-c-k-c0x00ffffff-no-rj",
+    thumbnail: thumbnail("rMWDtxJQIbQ"),
+    view: ViewsConverter(23057),
+    timeStamp: "05/11/2021"
+  },
+
+  {
+    _id: "43siOzkqGQg",
+    title: "Facebook JavaScript Interview Question | Learn Web Development Now",
+    videoLink: "https://www.youtube.com/watch?v=43siOzkqGQg",
+    description:
+      "Try to solve the question yourself first! https://jsfiddle.net/jadeallencook/dh...",
+    creator: "Jade Allen Cook",
+    categoryName: "Facebook",
+    img: "https://yt3.ggpht.com/RatStD09ZF4uR5j9nu5oCfv4dT7IEpU_YDLXse4y3ml3aRVCeE3pBAah6QlMP5Gc_ZvLZ0wyRQ=s176-c-k-c0x00ffffff-no-rj",
+    thumbnail: thumbnail("43siOzkqGQg"),
+    view: ViewsConverter(8262),
+    timeStamp: "01/18/2020"
   },
 ];
